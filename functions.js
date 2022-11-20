ca62a208f351593bfbdfb3a5ec87f4f062cd12c6
Roman-Zalutynski <smartweb.rz@gmail.com>
2022-11-20 21:50:22 +0100

    finished module 4 lesson 2

---
+++ functions.js
@@ -0,0 +1,12 @@
+// ! module 4 lesson 2
+// functions - always returns something. So we can work with returned value 
+//function
+function sum(a, b) {
+   return a + b;
+}
+console.log(sum(3, 5));
+
+// procedure - just do something inside the funtion and doesn't return any value;
+function sum2(a, b) {
+   console.log(a + b);
+}
